0699cc733fd4cc1bd2a0fa035f0d8e0b92635b3a
FearCR <39870104+FearCR@users.noreply.github.com>
2019-09-06 12:16:48 -0600

    Add files via upload

--- PiPorSeries/Buzon.h
+++ PiPorSeries/Buzon.h
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/*
- * File:   Buzon.h
- * Author: osboxes
- *
- * Created on September 12, 2018, 2:47 PM
- */
 
 #ifndef Buzon_H
 #define Buzon_H
