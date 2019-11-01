7007b06dccba7142f71107bb176d17ac60bbb821
FearCR <39870104+FearCR@users.noreply.github.com>
2019-11-01 15:10:24 -0600

    Add files via upload

--- TareaProgramada1_B62386/Collatz/Collatz-bin.cpp
+++ TareaProgramada1_B62386/Collatz/Collatz-bin.cpp
@@ -1,47 +1,38 @@
-
 #include <iostream>
+#include <bitset>
 using namespace std;
-double inicio;
-double final_;
-double tiempo;
-time_t timer;
 int pasos=0;
 
-// A method to multiply two numbers using Russian Peasant method
-
-unsigned int bitwiseSum(unsigned int num1, unsigned int num2){
-  unsigned int carry;
-  while (num2 != 0) {
-      carry = num1 & num2;
-      num1 = num1 ^ num2;
-      num2 = carry << 1;
-   }
-   return num1;
+int suma(unsigned int x, unsigned int y)
+{
+    while(y!=0)
+    {
+        int carry=x&y;
+        x=x^y;
+        y=carry<<1;
+        
+    }
+    return x;
 }
 
-
+void collatz(unsigned int numero){
+    unsigned int tmp=numero;
+    
+    while(tmp!=1){
+        if((tmp&1)==0){
+            tmp=tmp>>1;
+            pasos++;
+        }else{
+            unsigned int multiply=tmp<<1;
+            unsigned int add=multiply|1;
+            unsigned int third=suma(add,tmp);
+            tmp=third;
+        }
+        cout<<tmp<<endl;
+    }
+}
 
 int main()
 {
-  int a;
-  unsigned int num=123;
-  unsigned int mul;
-  unsigned int  adi;
-  unsigned int fin;
- while(num!=1){
-    cout<<num<<endl;
-    if(num%2==1){
-      mul=num<<1;
-      adi=num|1;
-      fin=bitwiseSum(num,adi);
-      num=fin;
-    }else{
-      num=num>>1;
-    }
-    cin>>a;
-  }
-  cout<<num<<endl;
-
-  cout << num << endl;
-  return 0;
+collatz(123);
 }
