a3a5743441a5b2d150ee51de32619c639941b574
FearCR <39870104+FearCR@users.noreply.github.com>
2019-11-08 12:44:00 -0600

    Add files via upload

--- TareaProgramada1_B62386/Ascensor/Ascensor.cpp
+++ TareaProgramada1_B62386/Ascensor/Ascensor.cpp
@@ -13,10 +13,12 @@ using namespace std;
 #define NUMTHRDS 100
 #define PISOS 16
 
-
 #define BLACK   "\033[0m"
 #define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
 #define BLUE    "\033[1;34m"
+#define CYAN    "\033[1;36m"
 
 int status;
 int n = 256;
@@ -71,7 +73,7 @@ int main ( int argc, char *argv[] ) {
 
 
 void *ascensor ( void *arg ) {
-  usleep(1000);//solo para asegurarse de que al ascensor entre la cantidad de personas que tienen que entrar al inicio, si no trabaja muy rapido y no se aprecia
+  // /usleep(1000);//solo para asegurarse de que al ascensor entre la cantidad maxima de personas que puede entrar al inicio, si no puede que trabaje muy rapido y no se aprecie.
   //con este metodo se puede apreciar bien al inicio del programa. Al final vuelve a alcanzar la velocidad de los threads atendiendolos justo cuando llegan.
   //tambien se puede poner un sleep entre la subida y bajada de cada piso para que el ascensor no supere la velocidad de los otros threads  y se sobrecargue
   long i;
@@ -89,7 +91,7 @@ void *ascensor ( void *arg ) {
           pthread_mutex_lock ( &mutex );
           cPersonaDentro--;
           bajadas[pisoActual]--;
-          cout<<"Bajo uno en el piso "<<pisoActual<<" adentro del ascensor hay "<<cPersonaDentro<<endl;
+          cout<<"Bajo uno en el piso "<<pisoActual<<" adentro del ascensor hay "<<cPersonaDentro<<" personas."<<endl;
           pthread_mutex_unlock ( &mutex );
         }else{
           break; //si se vacía el ascensor
@@ -103,11 +105,14 @@ void *ascensor ( void *arg ) {
           pthread_mutex_lock ( &mutex );
           cPersonaDentro++;
           cPersonaFuera--;
-          cout<<"subio uno en el piso "<<pisoActual<<" adentro del ascensor hay "<<cPersonaDentro<<endl;
+          cout<<"subio uno en el piso "<<pisoActual<<" adentro del ascensor hay "<<cPersonaDentro<<" personas."<<endl;
           subidas[pisoActual]--;
           pthread_mutex_unlock ( &mutex );
 
         }else{
+          pthread_mutex_lock ( &mutex );
+          cout<<RED"El ascensor esta lleno no puede entrar nadie mas hasta que al menos uno baje."<<BLACK<<endl;
+          pthread_mutex_unlock ( &mutex );
           break;//si no hay espacio sale del ciclo
         }
       }
@@ -115,13 +120,14 @@ void *ascensor ( void *arg ) {
 
     if(cPersonaFuera==0 && cPersonaDentro==0){
       pthread_mutex_lock ( &mutex );
-      cout<<"Nadie necesita subir o bajar del ascensor, entonces espero. "<<endl;
+      cout<<RED"Nadie necesita subir o bajar del ascensor, entonces espero. "<<BLACK<<endl;
       pthread_mutex_unlock ( &mutex );
       moving=false;
       sem_wait( &sAscensor );
-      moving=true;
+
       pthread_mutex_lock ( &mutex );
-      cout<<"señal recibida, alguien necesita subir al ascensor"<<endl;
+      moving=true;
+      cout<<GREEN"señal recibida, alguien necesita subir al ascensor"<<BLACK<<endl;
       pthread_mutex_unlock ( &mutex );
 
     }
@@ -147,46 +153,47 @@ void *ascensor ( void *arg ) {
       if(pisoActual<pisoMaximo||pisoActual<pisoMinimo){
         subiendo=true;
         pisoActual++;
-        //sem_post( &sPersona );
-        cout<<pisoActual<<endl;
-      }else{
-        subiendo=false;
+        pthread_mutex_lock ( &mutex );
+        cout<<CYAN"Piso: "<<pisoActual<<BLACK<<endl;
+        pthread_mutex_unlock ( &mutex);
+        }else{
+          subiendo=false;
+        }
       }
-    }
-    if(!subiendo){
-      if(pisoActual>pisoMinimo||pisoActual>pisoMaximo){
-        subiendo=false;
-        pisoActual--;
-        //sem_post( &sPersona );
-        cout<<pisoActual<<endl;
-      }else{
-        subiendo=true;
+      if(!subiendo){
+        if(pisoActual>pisoMinimo||pisoActual>pisoMaximo){
+          subiendo=false;
+          pisoActual--;
+          pthread_mutex_lock ( &mutex);
+          cout<<CYAN"Piso: "<<pisoActual<<BLACK<<endl;
+          pthread_mutex_unlock ( &mutex );
+          }else{
+            subiendo=true;
+          }
+        }
+        //cout<<cPersonaDentro;
       }
+      pthread_exit ( ( void * ) 0 );
     }
-    //cout<<cPersonaDentro;
-  }
-  pthread_exit ( ( void * ) 0 );
-}
 
 
-void *persona ( void *arg ) {
-  long i;
-  i = (long) arg;
-  int meSuboEn=rand()%16 +1;
-  int meBajoEn=rand()%16 +1;
-  pthread_mutex_lock ( &mutex );
-  cPersonaFuera++;
-  subidas[meSuboEn]++;
-  if(moving==false){
-      sem_post(&sAscensor);
-  }
-  pthread_mutex_unlock ( &mutex );
+    void *persona ( void *arg ) {
+      long i;
+      i = (long) arg;
+      int meSuboEn=rand()%16 +1;
+      int meBajoEn=rand()%16 +1;
+      pthread_mutex_lock ( &mutex );
+      cPersonaFuera++;
+      cout<<BLUE"Soy la persona "<<i<<", presione el boton para subir en el piso :"<<meSuboEn<<YELLOW", Y me bajo en el piso: "<<meBajoEn<<BLACK<<endl;
+      subidas[meSuboEn]++;
+      if(moving==false){
+        sem_post(&sAscensor);
+      }
+      pthread_mutex_unlock ( &mutex );
+      sem_wait(&sPersona);
+      pthread_mutex_lock ( &mutex );
+      bajadas[meBajoEn]++;
+      pthread_mutex_unlock ( &mutex );
 
-
-  sem_wait(&sPersona);
-  pthread_mutex_lock ( &mutex );
-  bajadas[meBajoEn]++;
-  pthread_mutex_unlock ( &mutex );
-
-  pthread_exit ( ( void * ) 0 );	// Finish this thread
-}
+      pthread_exit ( ( void * ) 0 );	// Finish this thread
+    }
