b70e6616d1db5a3517181819c1bf27ce2e3b6e63
FearCR <39870104+FearCR@users.noreply.github.com>
2019-11-22 12:52:13 -0600

    Add files via upload

--- TareaProgramada2_B62386/CollatzMPI.cpp
+++ TareaProgramada2_B62386/CollatzMPI.cpp
@@ -8,32 +8,37 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-    int size, rank;
+  int size, rank;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int globaldata[100];/*wants to declare array this way*/
-    int localdata[100/size];/*without using pointers*/
+  int globaldata[100];/*wants to declare array this way*/
+  int localdata[100/size];/*without using pointers*/
+  
+  int i;
+  if (rank == 0) {
 
-    int i;
-    if (rank == 0) {
-
-        for (i=0; i<100; i++){
-          globaldata[i] = rand()%1000+1;
-        }
-
-
+    for (i=0; i<100; i++){
+      globaldata[i] = rand()%1000+1;
     }
 
-    MPI_Scatter(globaldata, 100/size, MPI_INT, &localdata, 100/size, MPI_INT, 0, MPI_COMM_WORLD);
 
-//cout<<"soy el proceso "<<rank<<" dato: "<<localdata[10]<<endl;
-/*for (i=0; i<100/size; i++){
-  cout<<localdata[i]<<endl;
-}*/
-for (int i = 0; i <= 100/size; i++) {
+  }
+
+  MPI_Scatter(globaldata, 100/size, MPI_INT, &localdata, 100/size, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+  for (i=0; i<100/size; i++){
+    cout<<"proceso "<< rank<<" dato en la posicion "<<i<<" local: "<<localdata[i]<<endl;
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(rank==0){
+    cout<<endl<<endl<<endl;
+  }
+
+  for (int i = 0; i <= 100/size; i++) {
     while(localdata[i]>1){
       if(fmod(localdata[i],2)==0){
         localdata[i]=localdata[i]/2;
@@ -43,16 +48,16 @@ for (int i = 0; i <= 100/size; i++) {
     }
   }
 
-    MPI_Gather(&localdata, 100/size, MPI_INT, globaldata, 100/size, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Gather(&localdata, 100/size, MPI_INT, globaldata, 100/size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
+  if (rank == 0) {
 
-        for (i=0; i<100; i++){
-          cout<<globaldata[i]<<endl;
-        }
-      }
+    for (i=0; i<100; i++){
+      cout<<"dato en la posicion "<<i<<" global: "<<globaldata[i]<<endl;
+    }
+  }
 
 
-    MPI_Finalize();
-    return 0;
+  MPI_Finalize();
+  return 0;
 }
