3c224540188105bc7a304bb6372d441770ec8447
FearCR <39870104+FearCR@users.noreply.github.com>
2019-12-06 12:39:52 -0600

    Add files via upload

---
+++ Examen-2/Pregunta3/client.cpp
@@ -0,0 +1,19 @@
+#include "Socket.h"
+int main(){
+  string ip ="localhost";
+  string port = "9876";
+  string write;
+  while(write!="exit"){
+  std::cout << "Escriba el mensaje que quiere que se devuelva por el mirror server, solo se pasan 1024 bytes, escriba exit para salir"  << '\n';
+  std::cin>>write;
+  Socket *sock = new Socket(AF_INET,SOCK_STREAM,0); //AF_INET (Internet mode) SOCK_STREAM (TCP mode) 0 (Protocol any)
+  string buffer;
+  sock->connect(ip, port);
+  //sock->socket_write("GET / HTTP/1.1\r\nhost: redes.ecci\r\n\r\n");
+  sock->socket_write(write);
+
+  sock->socket_read(buffer, 1024);
+  cout << buffer << endl;
+}
+  return 0;
+}
---
+++ Examen-2/Pregunta3/server.cpp
@@ -0,0 +1,73 @@
+#include "Socket.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+double col(double x){
+  double numero=x;
+  if(fmod(numero,2)==0){
+    numero=numero/2;
+  }else{
+    numero=(3*numero)+1;
+
+  }
+  return numero;
+}
+int main(){
+  string ip = "localhost";
+  string port = "9876";
+  Socket *socket1 = new Socket(AF_INET,SOCK_STREAM,0);
+  int optVal = 1;
+  socket1->socket_set_opt(SOL_SOCKET, SO_REUSEADDR, &optVal);
+  socket1->bind(ip, port);
+  socket1->listen(10);
+  std::cout<<"inicia el servidor"<<std::endl;
+while(true){//este while se puede quitar y se quita el comentario de shutdown y close en socket 2
+  while (true) {
+    vector<Socket> lee(1);
+    lee[0] = *socket1;
+    int seconds = 10;
+    if(Socket::select(&lee, NULL, NULL, seconds) < 1){
+      //No hay coneccion
+      continue;
+    }else{
+      //algo se recivio
+      break;
+    }
+  }
+  Socket *socket2 = socket1->accept();
+
+  while (true) {
+    vector<Socket> lee(1);
+    lee[0] = *socket2;
+    int seconds = 10;
+    if(Socket::select(&lee, NULL, NULL, seconds) < 1){
+      //No hay Input
+      continue;
+    }else{
+      string buffer;
+      socket2->socket_read(buffer, 1024);
+      stringstream str(buffer);
+      double numero=0;
+      str>>numero;
+      int pasos;
+      while(numero!=1){
+      numero=col(numero);
+      pasos++;
+  }
+  buffer=std::to_string(pasos);
+      socket2->socket_write(buffer);
+      break;
+    }
+  }
+}
+  //socket2->socket_shutdown(2);
+  //socket2->close();
+
+  socket1->socket_shutdown(2);
+  socket1->close();
+
+
+  return  0;
+}
